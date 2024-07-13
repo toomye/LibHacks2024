fd153d1a4ac48aa416b71540d513ac82796a0890
tyler <tyler_lee_2@sfu.ca>
2024-07-13 14:32:59 -0700

    fixed caesar added curl download
    added text files for encryption
    added part of curl adder still testing

--- secure_file_transfer.cpp
+++ secure_file_transfer.cpp
@@ -1,19 +1,20 @@
 #include "caesar.hpp"
 #include "curl_downloader_alt.hpp"
-#include "hash_cpp.h"
+//#include "hash_cpp.h"
 #include <iostream>
 using namespace std;
 
 int main() {
     
     string src, dest;
+
+    int shift=3; //sets shift value
+    int option = 1; // 1 to decode 0 to encode
     
     cout << "Enter source file name: ";
     cin >> src;
     cout << "Enter target file name: ";
     cin >> dest;
-    cout << "Enter the shift amount: ";
-    cin >> shift;
 
     int lines_read = 0;
 
@@ -24,6 +25,18 @@ int main() {
     }
 
     std::cout << "Read " << lines_read << " lines" << std::endl;
+
+    string url_str = "encode.txt";
+	std::string file_output_name = "downloaded_encodedfile.txt";
+	if (vix::curl_download(file_output_name, url_str)) {
+		std::cout<<"File "<<file_output_name<<" was downloaded!"<<std::endl;
+	} else {
+		std::cout<<"File "<<file_output_name<< "was not downloaded"<<std::endl;
+	}
+
+
+
+
 
     return 0;
 }
