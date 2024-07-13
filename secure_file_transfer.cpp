c7e13026c9ba468ca8fc61e8f0ac3caaea73a08f
tyler <tyler_lee_2@sfu.ca>
2024-07-13 15:28:46 -0700

    grab url and encode
    use curl downloader and caesar cipher

--- secure_file_transfer.cpp
+++ secure_file_transfer.cpp
@@ -1,13 +1,14 @@
 #include "caesar.hpp"
 #include "curl_downloader_alt.hpp"
-//#include "hash_cpp.h"
+#include "hash_cpp.h"
 #include <iostream>
 using namespace std;
 
 int main() {
     
+    
+/*
     string src, dest;
-
     int shift=3; //sets shift value
     int option = 1; // 1 to decode 0 to encode
     
@@ -25,17 +26,25 @@ int main() {
     }
 
     std::cout << "Read " << lines_read << " lines" << std::endl;
-
-    string url_str = "encode.txt";
-	std::string file_output_name = "downloaded_encodedfile.txt";
+*/
+    string url_str = "https://www.gutenberg.org/cache/epub/11/pg11.txt";
+	std::string file_output_name = "downloaded_file.txt";
 	if (vix::curl_download(file_output_name, url_str)) {
 		std::cout<<"File "<<file_output_name<<" was downloaded!"<<std::endl;
 	} else {
 		std::cout<<"File "<<file_output_name<< "was not downloaded"<<std::endl;
 	}
 
-
-
+    
+    string src, dest;
+    cout << "Enter source file name: ";
+    cin >> src;
+    cout << "Enter target file name: ";
+    cin >> dest;
+    int shift=3; //sets shift value
+    int option = 1; // 1 to decode 0 to encode
+    int lines_read = 0;
+    lines_read = arya::encode_to_file(dest, src, shift);
 
 
     return 0;
